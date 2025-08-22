58be82b5b41ea89c2341f70e4b4318c8cd1e76f1
Dorian Wiegner <dorian.wiegner@hin.ch>
2025-08-22 17:21:04 +0200

    feature(SE-1467): fix MedicalProblemDTO setComments issue

--- Implementation/fhir-adapter/src/main/java/ch/fhir/epr/adapter/data/dto/MedicalProblemDTO.java
+++ Implementation/fhir-adapter/src/main/java/ch/fhir/epr/adapter/data/dto/MedicalProblemDTO.java
@@ -1,15 +1,15 @@
 /**
  * Copyright (c) 2022 eHealth Suisse
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
  * associated documentation files (the “Software”), to deal in the Software without restriction,
  * including without limitation the rights to use, copy, modify, merge, publish, distribute,
  * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- *
+ * <p>
  * The above copyright notice and this permission notice shall be included in all copies or
  * substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
  * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
  * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
@@ -18,13 +18,9 @@
  */
 package ch.fhir.epr.adapter.data.dto;
 
+import lombok.*;
+
 import java.time.LocalDate;
-import java.util.List;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -32,29 +28,29 @@ import lombok.ToString;
 @Setter
 @ToString
 public class MedicalProblemDTO extends BaseDTO {
-  private ValueDTO clinicalStatus;
-  private ValueDTO verificationStatus;
-  private LocalDate begin;
-  private LocalDate end;
-  private LocalDate recordedDate;
+    private ValueDTO clinicalStatus;
+    private ValueDTO verificationStatus;
+    private LocalDate begin;
+    private LocalDate end;
+    private LocalDate recordedDate;
 
-  public MedicalProblemDTO(String id, ValueDTO code, ValueDTO clinicalStatus, ValueDTO verificationStatus,
-      LocalDate recordedDate, LocalDate begin, LocalDate end, HumanNameDTO recorder, List<CommentDTO> comments,
-      String organization) {
-    this.begin = begin;
-    this.end = end;
-    this.clinicalStatus = clinicalStatus;
-    this.verificationStatus = verificationStatus;
-    this.recordedDate = recordedDate;
-    setId(id);
-    setCode(code);
-    setRecorder(recorder);
-    setOrganization(organization);
-    setComments(comments);
-  }
+    public MedicalProblemDTO(String id, ValueDTO code, ValueDTO clinicalStatus, ValueDTO verificationStatus,
+                             LocalDate recordedDate, LocalDate begin, LocalDate end, HumanNameDTO recorder, CommentDTO comment,
+                             String organization) {
+        this.begin = begin;
+        this.end = end;
+        this.clinicalStatus = clinicalStatus;
+        this.verificationStatus = verificationStatus;
+        this.recordedDate = recordedDate;
+        setId(id);
+        setCode(code);
+        setRecorder(recorder);
+        setOrganization(organization);
+        setComment(comment);
+    }
 
-  @Override
-  public LocalDate getDateOfEvent() {
-    return getRecordedDate();
-  }
+    @Override
+    public LocalDate getDateOfEvent() {
+        return getRecordedDate();
+    }
 }
